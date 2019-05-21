27809f81dc069f496c99380616512b93747732e2
Ben Lewis <hello@blewis.co.uk>
2019-05-21 21:45:46 +0300

    Add the first working example

--- examples/1/index.js
+++ examples/1/index.js
@@ -2,8 +2,8 @@ const a = 1
 a = 2 // This should override the assignment
 console.log(a) // Should print 2
 
-const arrowFcn = a, b => a + b;
+const arrowFcn = (a, b) => a + b;
 console.log(arrowFcn(1, 2))
 
-setTimeout(console.log('Hello'), 1500) // Should print 'hello' after 1.5 seconds
+setTimeout(() => console.log('Hello'), 1500) // Should print 'hello' after 1.5 seconds
 
